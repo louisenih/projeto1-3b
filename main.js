62cbd77936ddb7d682ecfc993f7073bb698c9561
louisenih <162455023+louisenih@users.noreply.github.com>
2024-04-30 07:41:18 -0300

    d

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const botoes = document.querySelectorAll(".botao")
 const textos = document.querySelectorAll(".aba-conteudo")
-const contador = document.querySelectorAll(".contador")
+
 
 for(let i=0; i<botoes.length;i++){
     botoes[i].onclick=function(){
@@ -22,5 +22,5 @@ for(let j=0;j<botoes.length;j++){
 
 
 
-
+const contador = document.querySelectorAll(".contador")
 
