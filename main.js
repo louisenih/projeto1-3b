cbd0a5b1235796c0024e6d1a468130280d78369d
louisenih <162455023+louisenih@users.noreply.github.com>
2024-05-07 07:45:29 -0300

    fg.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const botoes = document.querySelectorAll(".botao")
-const textos = document.querySelectorAll(".aba-conteudo")
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
 
 for(let i=0; i<botoes.length;i++){
@@ -7,12 +7,12 @@ for(let i=0; i<botoes.length;i++){
 
 for(let j=0;j<botoes.length;j++){
     botoes[j].classList.remove('ativo');
-    textos[j].classList.remove('ativo')
+    textos[j].classList.remove('ativo');
 }
 
 
         botoes[i].classList.add('ativo');
-        textos[i].classList.add('ativo')
+        textos[i].classList.add('ativo');
     }
 }
    
