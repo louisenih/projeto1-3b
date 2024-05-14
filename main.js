92f2c7ee6380149cd904ca57179d19a83936705e
louisenih <162455023+louisenih@users.noreply.github.com>
2024-05-14 07:54:18 -0300

    nn

--- main.js
+++ main.js
@@ -34,10 +34,14 @@ let dias = Math.floor(horas / 24);
 
 segundos %=60;
 minutos %=60;
+horas %=24;
 
 return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
 
 }
+
+
+
 
 
 
